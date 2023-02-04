b4c0891bb9342aa912cf2f7680856d4f07ae1e5b
Andre Oliveira <122166590+oliveira-96@users.noreply.github.com>
2023-02-03 21:32:07 -0300

    aa
    aa

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+// Dados fixos
+define('BLOG_NOME', 'Projeto X');
+define('BLOG_AUTOR', 'André Oliveira');
+define('BLOG_SOBRE', 'André Oliveira é um programador que ensina seus alunos a desenvolver sites!');
+?>
